897318a3f9d42480c74df797dca179ca2ef9919f
Salvador Dieffenbach <zogwarg@hotmail.com>
2018-12-31 01:32:55 -0500

    Basic working assignment

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+let topics = [
+    "aerial",
+    "architectural",
+    "candid",
+    "documentary",
+    "fashion",
+    "food",
+    "landscape",
+    "nature",
+    "night",
+    "photojournalism",
+    "portrait",
+    "sports",
+    "street",
+    "war",
+    "wildlife"
+];
+
+const gifMaker = {
+   
+    createButtons() {
+        topics.forEach((element) => {
+            let topicBtn = $("<button>");
+            topicBtn.addClass("button topic-button");
+            topicBtn.attr("data-topic", element);
+            topicBtn.text(element);
+            $(".one").append(topicBtn);
+        });
+        $(".topic-button").on("click", function() {
+            gifMaker.getGifs($(this));
+        });
+    },
+
+    getGifs(thisTopic) {
+        $(".two").empty();
+        let topic = thisTopic.attr("data-topic");
+        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=C3B2bBZNWCN7FbYAWf7CfSZNN1CRAMl6&limit=10`
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            let i = 1;
+            response.data.forEach((element) => {
+                let imageCard = $("<div>");
+                imageCard.addClass("image-card");
+                $(".two").append(imageCard);
+
+                let topicImage = $("<img>");
+                topicImage.attr("src", element.images.fixed_width_still.url);
+                topicImage.attr("data-still", element.images.fixed_width_still.url);
+                topicImage.attr("data-animate", element.images.fixed_width.url);
+                topicImage.attr("data-state","still");
+                topicImage.addClass("gif");
+                $(`.image-card:nth-of-type(${i})`).append(topicImage);
+
+                let ratingParagraph = $("<p>");
+                ratingParagraph.addClass("rating");
+                ratingParagraph.text(element.rating);
+                $(`.image-card:nth-of-type(${i})`).append(ratingParagraph);
+                i++;
+            });
+            $(".gif").on("click", function() {
+                gifMaker.toggleGifState($(this));
+            });
+        });
+    },
+
+    toggleGifState(thisGif) {
+        if(thisGif.attr("data-state") === "still") {
+            thisGif.attr("src", thisGif.attr("data-animate"));
+            thisGif.attr("data-state","animate");
+        } else if(thisGif.attr("data-state") === "animate") {
+            thisGif.attr("src", thisGif.attr("data-still"));
+            thisGif.attr("data-state","still"); 
+        }
+
+    }
+}
+
+$(document).ready(() => {
+    gifMaker.createButtons();
+});
